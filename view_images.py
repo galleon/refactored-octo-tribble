278a6878195aaeac6158075a622cfde62292b6b1
Santiago Pighin <pighin.santiago@gmail.com>
2021-07-07 15:30:05 +0200

    geting patient_id and number of slices

--- view_images.py
+++ view_images.py
@@ -1,18 +1,48 @@
 import streamlit as st
+import requests
 
 import numpy as np
 import pandas as pd
 
-st.markdown("""# This is a header
-## This is a sub header
-This is text""")
 
-df = pd.DataFrame({
-          'first column': list(range(1, 11)),
-          'second column': np.arange(10, 101, 10)
-        })
+url = 'https://drmaboule-ouh2vqtouq-ew.a.run.app/patients'
 
-# this slider allows the user to select a number of lines
-# to display in the dataframe
-# the selected value is returned by st.slider
-line_count = st.slider('Select a line count', 1, 10, 3)
+
+def get_patient_id():
+    st.markdown("""get patient""")
+    r = requests.get(url).json()
+    list_of_patients = r['patients']
+#    print(list_of_patients)
+    patient_id = st.selectbox("patient", list_of_patients, help="select the patient id")
+    return patient_id
+
+def get_slices(patient_id):
+    st.markdown("""get slice number""")
+    params={'patient_id':patient_id}
+    r = requests.get(url+'/'+patient_id).json()
+    number_of_slices = r['number_of_slices']
+    return number_of_slices
+
+
+patient_id = get_patient_id()
+number_of_slices = get_slices(patient_id)
+slice_number = st.slider('Select a line count', 1, number_of_slices, 1)
+
+
+
+
+@st.cache
+def get_select_box_data():
+    print('get_select_box_data called')
+    return pd.DataFrame({
+          'first column': list(range(5, 11)),
+          'second column': np.arange(5, 11, 1)
+         })
+
+# df = get_select_box_data()
+
+# option = st.selectbox('Select a line to filter', df['first column'])
+
+# filtered_df = df[df['first column'] == option]
+
+# st.write(filtered_df)
